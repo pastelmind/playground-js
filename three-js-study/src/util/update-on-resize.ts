10a95d85bf13b2bfb47063a914375074ae255b84
Yehyoung Kang <keepyourhonor+github@gmail.com>
2023-04-21 02:13:02 +0900

    Add Ch1 Porsche example page

---
+++ three-js-study/src/util/update-on-resize.ts
@@ -0,0 +1,13 @@
+import type * as THREE from "three";
+
+export const onResize = (
+  camera: THREE.PerspectiveCamera,
+  renderer: THREE.Renderer
+) => {
+  const resizer = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  };
+  window.addEventListener("resize", resizer, false);
+};
